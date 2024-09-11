3d44a045108524247306383f3cf0c037dee99b73
Simone Albano <dc.jest@gmail.com>
2024-09-11 11:51:39 +0200

    Update weather.meta.js

--- weather.meta.js
+++ weather.meta.js
@@ -2,12 +2,12 @@
 // @id             iitc-plugin-weather
 // @name           IITC plugin: Weather Map
 // @category       Layer
-// @version        0.1.8.20160726.001
+// @version        0.1.8.20240911.001
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @updateURL      https://github.com/Hurqalia/weather_map/raw/master/weather.meta.js
-// @downloadURL    https://github.com/Hurqalia/weather_map/raw/master/weather.user.js
-// @installURL     https://github.com/Hurqalia/weather_map/raw/master/weather.user.js
-// @description    [hurqalia22-2016-07-26-001] Weather Map
+// @updateURL      https://github.com/DrJest/weather_map/raw/master/weather.meta.js
+// @downloadURL    https://github.com/DrJest/weather_map/raw/master/weather.user.js
+// @installURL     https://github.com/DrJest/weather_map/raw/master/weather.user.js
+// @description    [DrJest-2024-09-11-001] Weather Map
 // @include        https://*.ingress.com/intel*
 // @include        http://*.ingress.com/intel*
 // @match          https://*.ingress.com/intel*
